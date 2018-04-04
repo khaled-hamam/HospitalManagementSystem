11bd6c7a54af406e0408651fd5ec6208e6ed45e3
Karim <karim.abdelghaffar98@gmail.com>
2018-04-04 18:42:37 +0200

    person/patient-classes

---
+++ HospitalManagementSystem/Models/AppointmentPatient.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagementSystem.Models
+{
+    class AppointmentPatient : Patient
+    {
+        // member variables
+        private List<Appointment> appointments { get; set; }
+        //constructors
+        public AppointmentPatient() : base()
+        {
+            appointments = new List<Appointment>();
+        }
+        public AppointmentPatient(string id, string name, DateTime dt, string address, string diagnosis) : base(id, name, dt, address, diagnosis)
+        {
+            appointments = new List<Appointment>();
+        }
+        // member methods
+        public void addAppointment(Appointment apm)
+        {
+            appointments.Add(apm);
+        }
+        public void removeAppointment(string id)
+        {
+            for (int i = 0; i < appointments.Count; i++)
+            {
+                if (appointments[i].id == id)
+                {
+                    appointments.Remove(appointments[i]);
+                    return;
+                }
+            }
+        }
+        public override float getBill()
+        {
+            int duration = 0;
+            for (int i = 0; i < appointments.Count; i++)
+            {
+                duration += appointments[i].duration;
+            }
+            float bill = duration + (float)(duration * 0.5);
+            return bill;
+        }
+    }
+}
---
+++ HospitalManagementSystem/Models/ResidentPatient.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagementSystem.Models
+{
+    class ResidentPatient : Patient
+    {
+        // member variables
+        private Room room { get; set; }
+        private List<Medicine> history { get; set; }
+        public int duration { get; set; }
+        //constructors
+        public ResidentPatient() : base()
+        {
+            this.room = new Room();
+            this.history = new List<Medicine>();
+        }
+        public ResidentPatient(string id, string name, DateTime dt, string address, string diagnosis, Room room, int duration) : base(id, name, dt, address, diagnosis)
+        {
+
+            this.room = room;
+            this.duration = duration;
+            this.history = new List<Medicine>();
+        }
+        // member methods
+        public void addMedicine(Medicine M)
+        {
+            history.Add(M);
+        }
+        public override float getBill()
+        {
+            float bill = this.duration + (float)(this.duration * 0.5);
+            return bill;
+        }
+    }
+}
