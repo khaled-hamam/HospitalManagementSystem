95e79540eca28d01aeef75322c64ab10d2b7b440
mariamkamel <mariammkamel98@gmail.com>
2018-04-04 19:24:06 +0200

    Employee,Doctor,Nurse classes Added

---
+++ HospitalManagementSystem/Models/Nurse.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagementSystem.Models
+{
+    class Nurse : Employee
+    {
+        private List<Room> rooms;
+        private List<Patient> patients;
+        private Department department;
+
+        public void addRoom(Room room)
+        {
+            rooms.Add(room);
+        }
+
+        public void removeRoom(string id)
+        {
+            foreach (Room room in rooms)
+            {
+                if (room._id == id)
+                {
+                    rooms.Remove(room);
+                    break;
+                }
+            }
+        }
+
+        public void addPatient(Patient patient)
+        {
+            patients.Add(patient);
+        }
+
+        public void removePatient(string id)
+        {
+            foreach (Patient patient in patients)
+            {
+                if (patient._id == id)
+                {
+                    patients.Remove(patient);
+                    break;
+                }
+            }
+        }
+    }
+}
