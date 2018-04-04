ae3db7fadf7aa1d73daa75cbf3ca2565be800e1e
khaled-hamam <Khal3d.Mohamed@gmail.com>
2018-04-04 12:04:15 +0200

    Added Medicine/Appointment Model Classes

---
+++ HospitalManagementSystem/Models/Appointment.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagementSystem.Models
+{
+    class Appointment
+    {
+        private string id { get; set; }
+        private Doctor doctor { get; set; }
+        private Patient patient { get; set; }
+        private DateTime date { get; set; }
+        private int duration { get; set; }
+
+        public Appointment()
+        {
+            this.id = Guid.NewGuid().ToString();
+            this.doctor = new Doctor();
+            this.patient = new Patient();
+            this.date = new DateTime();
+            this.duration = 0;
+        }
+
+        public Appointment(Doctor doctor, Patient patient, DateTime date, int duration)
+        {
+            this.id = Guid.NewGuid().ToString();
+            this.doctor = doctor;
+            this.patient = patient;
+            this.date = date;
+            this.duration = duration;
+        }
+
+        public void cancel()
+        {
+            this.doctor.removeAppointment(this.id);
+            this.patient.removeAppointment(this.id);
+        }
+    }
+}
---
+++ HospitalManagementSystem/Models/Medicine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagementSystem.Models
+{
+    class Medicine
+    {
+        private string id { get; set; }
+        private string name { get; set; }
+        private DateTime startingDate { get; set; }
+        private DateTime endingDate { get; set; }
+
+        public Medicine()
+        {
+            this.id = Guid.NewGuid().ToString();
+            this.name = "";
+            this.startingDate = new DateTime();
+            this.startingDate = new DateTime();
+        }
+
+        public Medicine(string name, DateTime startingDate, DateTime endingDate)
+        {
+            this.id = Guid.NewGuid().ToString();
+            this.name = name;
+            this.startingDate = startingDate;
+            this.endingDate = endingDate;
+        }
+    }
+}
