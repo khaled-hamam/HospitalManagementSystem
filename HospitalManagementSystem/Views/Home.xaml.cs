84665bfb9881c8c67341ab974979620e05cd0648
khaled-hamam <Khal3d.Mohamed@gmail.com>
2018-04-04 12:01:16 +0200

    Fixed namespace error

--- HospitalManagementSystem/Views/Home.xaml.cs
+++ HospitalManagementSystem/Views/Home.xaml.cs
@@ -13,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace HospitalManagementSystem
+namespace HospitalManagementSystem.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Home.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Home : Window
     {
-        public MainWindow()
+        public Home()
         {
             InitializeComponent();
         }
