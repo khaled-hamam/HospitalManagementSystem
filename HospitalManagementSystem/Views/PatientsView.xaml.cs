1c78e41c9b6689cc3fa943639590a6e42a342ad2
karimelsayed98 <karim.abdelghaffar98@gmail.com>
2018-04-07 12:43:41 +0200

    Add views and viewmodels

--- HospitalManagementSystem/Views/PatientsView.xaml.cs
+++ HospitalManagementSystem/Views/PatientsView.xaml.cs
@@ -1,5 +1,4 @@
-﻿using HospitalManagementSystem.ViewModels;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,23 +16,13 @@ using System.Windows.Shapes;
 namespace HospitalManagementSystem.Views
 {
     /// <summary>
-    /// Interaction logic for Home.xaml
+    /// Interaction logic for PatientsView.xaml
     /// </summary>
-    public partial class Home : Window
+    public partial class PatientsView : UserControl
     {
-        public Home()
+        public PatientsView()
         {
             InitializeComponent();
-        }
-
-        private void DepartmentButton_Selected(object sender, RoutedEventArgs e)
-        {
-            DataContext = new DepartmentsViewModel();
-        }
-
-        private void PatientsButton_Selected(object sender, RoutedEventArgs e)
-        {
-            DataContext = new PatientsViewModel();
         }
     }
 }
