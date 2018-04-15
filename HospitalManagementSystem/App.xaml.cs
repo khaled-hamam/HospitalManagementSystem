bd6dfdd03f01f77389d1f0786b5cef98b82838de
khaled-hamam <Khal3d.Mohamed@gmail.com>
2018-04-15 18:18:04 +0200

    Initializing Hospital Instance

--- HospitalManagementSystem/App.xaml.cs
+++ HospitalManagementSystem/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HospitalManagementSystem.Models;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,9 @@ namespace HospitalManagementSystem
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            Hospital hospital = new Hospital();
+        }
     }
 }
