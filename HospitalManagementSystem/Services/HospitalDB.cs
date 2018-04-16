35425ef5f5266cb0eb629ae52251102e26a2d517
khaled-hamam <Khal3d.Mohamed@gmail.com>
2018-04-16 17:42:44 +0200

    Implemented FetchDepartments

--- HospitalManagementSystem/Services/HospitalDB.cs
+++ HospitalManagementSystem/Services/HospitalDB.cs
@@ -1,25 +1,54 @@
 ﻿using System;
 using MySql.Data.MySqlClient;
 using System.Data;
+using System.Collections.Generic;
+using HospitalManagementSystem.Models;
 
 namespace HospitalManagementSystem.Services
 {
-    public static class HospitalDB
+    static class HospitalDB
     {
-        public static MySqlConnection initConnection()
+        public static MySqlConnection InitConnection()
         {
             String connectionString = $"server={Credentials.Server};" +
                 $"user id={Credentials.UserID};" +
                 $"password={Credentials.Password};" +
                 $"database={Credentials.Database}";
 
-            using (MySqlConnection con = new MySqlConnection(connectionString))
-            {
-                if (con.State == ConnectionState.Closed)
-                    con.Open();
+            return new MySqlConnection(connectionString);
+        }
 
-                return con;
+        public static Dictionary<String, Department> FetchDepartments()
+        {
+            MySqlConnection con = InitConnection();
+            Dictionary<String, Department> departments = new Dictionary<String, Department>();
+            try
+            {
+                con.Open();
+                String query = "SELECT * FROM department";
+                MySqlCommand command = new MySqlCommand(query, con);
+                MySqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    departments.Add(reader.GetString("department_id"), 
+                        new Department
+                        {
+                            ID = reader.GetString("department_id"),
+                            Name = reader.GetString("name")
+                        }
+                    );
+                }
             }
+            catch
+            {
+                Console.WriteLine("Error Occured Fetching Departments.");
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            return departments;
         }
     }
 }
