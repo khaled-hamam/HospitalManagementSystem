fa46409682a6afd8cf7df5cedd62a7ff318e7695
khaled-hamam <Khal3d.Mohamed@gmail.com>
2018-04-16 15:53:20 +0200

    Configured the MySQL Server Connection

---
+++ HospitalManagementSystem/Services/Credentials.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HospitalManagementSystem.Services
+{
+    public static class Credentials
+    {
+        public const String Server = "*****";
+        public const String UserID = "*****";
+        public const String Password = "*****";
+        public const String Database = "*****";
+    }
+}
---
+++ HospitalManagementSystem/Services/HospitalDB.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace HospitalManagementSystem.Services
+{
+    public static class HospitalDB
+    {
+        public static MySqlConnection initConnection()
+        {
+            String connectionString = $"server={Credentials.Server};" +
+                $"user id={Credentials.UserID};" +
+                $"password={Credentials.Password};" +
+                $"database={Credentials.Database}";
+
+            using (MySqlConnection con = new MySqlConnection(connectionString))
+            {
+                if (con.State == ConnectionState.Closed)
+                    con.Open();
+
+                return con;
+            }
+        }
+    }
+}
