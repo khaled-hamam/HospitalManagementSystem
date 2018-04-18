ab979c549cf5dbdd341a52401a673fdee31a1528
mariamkamel <mariammkamel98@gmail.com>
2018-04-18 19:30:54 +0200

    Employee UI Validation Added

--- HospitalManagementSystem/ViewModels/EmployeesViewModel.cs
+++ HospitalManagementSystem/ViewModels/EmployeesViewModel.cs
@@ -1,14 +1,38 @@
 ﻿using HospitalManagementSystem.Models;
 using System.Collections.ObjectModel;
+using System;
 
 namespace HospitalManagementSystem.ViewModels
 {
     public class EmployeesViewModel : BaseViewModel
     {
+        public String EmployeeNameTextBox { get; set;  }
+        public String EmployeeAddressTextBox { get; set; }
+        public String EmployeeBirthDate { get; set; }
+        public String EmployeeSalaryTextBox { get; set; }
+        public String EmployeeDepartment { get; set; }
+        public String EmployeeRole { get; set; }
         public ObservableCollection<EmployeeCardViewModel> Employees { get; set; }
 
+        public bool ValidateName()
+        {
+            EmployeeNameTextBox = (EmployeeNameTextBox != null) ? EmployeeNameTextBox.Trim() : "";
+            EmployeeAddressTextBox = (EmployeeAddressTextBox != null) ? EmployeeAddressTextBox : "";
+            EmployeeSalaryTextBox = (EmployeeSalaryTextBox != null) ? EmployeeSalaryTextBox.Trim() : "";
+            EmployeeDepartment = (EmployeeDepartment != null) ? EmployeeDepartment.Trim() : "";
+            EmployeeRole = (EmployeeRole != null) ? EmployeeRole.Trim() : "";
+            if (EmployeeNameTextBox == "") return false;
+            if (EmployeeAddressTextBox == "") return false;
+            if (EmployeeBirthDate == "") return false;
+            if (EmployeeSalaryTextBox == "") return false;
+            if (EmployeeDepartment == "") return false; 
+            if( EmployeeRole=="") return false;
+            return true;
+        }
+        
         public EmployeesViewModel()
         {
+
             Employees = new ObservableCollection<EmployeeCardViewModel>();
             foreach (Employee employee in Hospital.Employees)
             {
