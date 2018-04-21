7a16e08ca0a1d984afab06e32d8ca37406d52568
khaled-hamam <Khal3d.Mohamed@gmail.com>
2018-04-21 21:32:52 +0200

    Initializing Employees from DB

--- HospitalManagementSystem/Models/Hospital.cs
+++ HospitalManagementSystem/Models/Hospital.cs
@@ -10,25 +10,88 @@ namespace HospitalManagementSystem.Models
 {
     class Hospital
     {
-        private static Dictionary<String, Employee> employees;
-        private static Dictionary<String, Patient> patients;
-        private static Dictionary<String, Appointment> appointments;
-        private static Dictionary<String, Department> departments;
-        private static Dictionary<String, Room> rooms;
-
-        public static Dictionary<String, Employee> Employees { get { return employees; } }
-        public static Dictionary<String, Patient> Patients { get { return patients; } }
-        public static Dictionary<String, Appointment> Appointments { get { return appointments; } }
-        public static Dictionary<String, Department> Departments { get { return departments; } }
-        public static Dictionary<String, Room> Rooms { get { return rooms; } }
+        public static Dictionary<String, Employee> Employees { get; private set; }
+        public static Dictionary<String, Patient> Patients { get; private set; }
+        public static Dictionary<String, Appointment> Appointments { get; private set; }
+        public static Dictionary<String, Department> Departments { get; private set; }
+        public static Dictionary<String, Room> Rooms { get; private set; }
 
         public Hospital()
         {
-            employees = new Dictionary<String, Employee>();
-            patients = new Dictionary<String, Patient>();
-            appointments = new Dictionary<String, Appointment>();
-            departments = new Dictionary<String, Department>();
-            rooms = new Dictionary<String, Room>();
+            Console.WriteLine(typeof(PrivateRoom).ToString());
+            Employees = new Dictionary<String, Employee>();
+            Patients = new Dictionary<String, Patient>();
+            Appointments = new Dictionary<String, Appointment>();
+            Departments = new Dictionary<String, Department>();
+            Rooms = new Dictionary<String, Room>();
+        }
+
+        public void InitializeDepartments()
+        {
+            List<Department> departmentList = HospitalDB.FetchDepartments();
+            foreach (Department department in departmentList)
+            {
+                Departments.Add(department.ID, department);
+            }
+        }
+
+        public void InitializeRooms()
+        {
+            List<Room> roomList = HospitalDB.FetchRooms();
+            foreach (Room room in roomList)
+            {
+                Rooms.Add(room.ID, room);
+            }
+        }
+
+        public void InitializeEmployees()
+        {
+            List<Doctor> doctorList = HospitalDB.FetchDoctors();
+            foreach (Doctor doctor in doctorList)
+            {
+                String departmentID = HospitalDB.FetchEmployeeDepartment(doctor.ID);
+                doctor.Department = Departments[departmentID];
+                Departments[departmentID].addDoctor(doctor);
+                if (doctor.IsHead)
+                    Departments[departmentID].HeadID = doctor.ID;
+
+                Employees.Add(doctor.ID, doctor);
+            }
+
+            List<Nurse> nurseList = HospitalDB.FetchNurses();
+            foreach (Nurse nurse in nurseList)
+            {
+                String departmentID = HospitalDB.FetchEmployeeDepartment(nurse.ID);
+                List<String> roomsID = HospitalDB.FetchNurseRooms(nurse.ID);
+                nurse.Department = Departments[departmentID];
+                Departments[departmentID].addNurse(nurse);
+                foreach (String roomID in roomsID)
+                {
+                    nurse.addRoom(Rooms[roomID]);
+                    Rooms[roomID].addNurse(nurse);
+                }
+
+                Employees.Add(nurse.ID, nurse);
+            }
+        }
+
+        public void InitializePatients()
+        {
+            List<Patient> patientList = HospitalDB.FetchPatients();
+            foreach (Patient patient in patientList)
+            {
+                if (patient.GetType() == typeof(ResidentPatient))
+                {
+                    // Room
+
+                    // Nurses
+                }
+                else if (patient.GetType() == typeof(AppointmentPatient))
+                {
+                    // Appointment
+                }
+                Patients.Add(patient.ID, patient);
+            }
         }
     }
 }
