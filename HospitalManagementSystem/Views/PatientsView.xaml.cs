c96dc7de1117e5ae47a3bd9827982d90273e3296
karimelsayed98 <karim.abdelghaffar98@gmail.com>
2018-04-23 19:59:29 +0200

    finish Add Patient

--- HospitalManagementSystem/Views/PatientsView.xaml.cs
+++ HospitalManagementSystem/Views/PatientsView.xaml.cs
@@ -29,24 +29,22 @@ namespace HospitalManagementSystem.Views
             ViewModel = new PatientsViewModel();
             DataContext = ViewModel;
             InitializeComponent();
+            PatientRoomNumberComboBox.DisplayMemberPath = "Value";
+            PatientRoomNumberComboBox.SelectedValuePath = "Key";
+            PatientRoomNumberComboBox.ItemsSource = ViewModel.ComboBoxItems;
         }
         public void addPatient(object sender, RoutedEventArgs e)
         {
-            if (ViewModel.ValidateName() && ViewModel.ValidateAddress() && PatientBirthDatetDatePicker.SelectedDate.ToString() != "")
-            {
-                ViewModel.Patients.Add(
-                    new PatientCardViewModel
-                    {
-                        Name = "Name",
-                        Type = "resident",
-                        ShortDiagnosis = "fever2"
-                    }
-                );
-                // Closing the Dialog
-                DialogHost.CloseDialogCommand.Execute(addPatientDialaog, null);
-            } else {
-                MessageBox.Show("Invalid Entry");
-            }
-        }
+            if (!ViewModel.ValidateInput())
+            { MessageBox.Show("Invalid Entry"); return;}
+            ViewModel.addPatient();
+            // Closing the Dialog
+            DialogHost.CloseDialogCommand.Execute(addPatientDialaog, null);
+            PatientNameTextBox.Clear();
+            PatientAddressTextBox.Clear();
+            PatientBirthDatetDatePicker.SelectedDate = DateTime.Today;
+            PatientTypeComboBox.SelectedIndex = -1;
+        } 
+  
     }
 }
