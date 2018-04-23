480ac76031ae3ce5a54b206c6405e527ad0d0016
khaled-hamam <Khal3d.Mohamed@gmail.com>
2018-04-23 18:43:46 +0200

    Implemented Search in EmployeesView

--- HospitalManagementSystem/ViewModels/EmployeesViewModel.cs
+++ HospitalManagementSystem/ViewModels/EmployeesViewModel.cs
@@ -2,21 +2,34 @@
 using System.Collections.ObjectModel;
 using System;
 using System.Windows.Input;
+using System.Linq;
 
 namespace HospitalManagementSystem.ViewModels
 {
     public class EmployeesViewModel : BaseViewModel
     {
+        /// <summary>
+        /// Items Properties
+        /// </summary>
+        public ObservableCollection<EmployeeCardViewModel> Employees { get; set; }
+        public ObservableCollection<EmployeeCardViewModel> FilteredEmployees { get; set; }
+
+        /// <summary>
+        /// Search Bar Properties
+        /// </summary>
+        public String SearchQuery { get; set; }
+        
+        /// <summary>
+        /// Add Dialog Properites
+        /// </summary>
         public String EmployeeNameTextBox { get; set;  }
         public String EmployeeAddressTextBox { get; set; }
         public String EmployeeSalaryTextBox { get; set; }
         public String EmployeeDepartment { get; set; }
         public String EmployeeRole { get; set; }
-        public ObservableCollection<EmployeeCardViewModel> Employees { get; set; }
 
         public ICommand SearchAction { get; set; }
 
-        
         public EmployeesViewModel()
         {
             SearchAction = new RelayCommand(Search);
@@ -33,11 +46,20 @@ namespace HospitalManagementSystem.ViewModels
                     }
                 );
             }
+            FilteredEmployees = new ObservableCollection<EmployeeCardViewModel>(Employees);
         }
 
         private void Search()
         {
-            Console.WriteLine("Searching Not Yet Implemented...");
+            if (String.IsNullOrEmpty(SearchQuery))
+            {
+                FilteredEmployees = new ObservableCollection<EmployeeCardViewModel>(Employees);
+                return;
+            }
+
+            FilteredEmployees = new ObservableCollection<EmployeeCardViewModel>(
+                Employees.Where(employee => employee.Name.ToLower().Contains(SearchQuery))
+            );
         }
 
         public bool ValidateName()
