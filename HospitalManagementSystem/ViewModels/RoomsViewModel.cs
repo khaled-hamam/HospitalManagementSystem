e84213c451cd66c6363e7a139a80b67ecb280e3d
mariamkamel <mariammkamel98@gmail.com>
2018-04-25 20:37:20 +0200

    RoomViewModel bugs fixed

--- HospitalManagementSystem/ViewModels/RoomsViewModel.cs
+++ HospitalManagementSystem/ViewModels/RoomsViewModel.cs
@@ -27,7 +27,6 @@ namespace HospitalManagementSystem.ViewModels
         public String RoomType { get; set; }
 
         public ICommand SearchAction { get; set; }
-        public string RoomtTypeComboBox { get; set; }
 
         public RoomsViewModel()
         {
@@ -126,7 +125,7 @@ namespace HospitalManagementSystem.ViewModels
                 HospitalDB.InsertRoom(newSemiPrivateRoom);
             }
 
-            else if(RoomType == "Standard Ward")
+            else if(RoomType == "Standard Ward<")
             {
                 StandardWard newStandardWardRoom = new StandardWard
                 {
@@ -167,16 +166,10 @@ namespace HospitalManagementSystem.ViewModels
                 if (int.Parse(RoomNumber) == room.RoomNumber)
                     return false;
             }
-
-
             
             return true;
         }
 
-        public bool ValidateRoomtType()
-        {
-            RoomtTypeComboBox = (RoomtTypeComboBox != null) ? RoomtTypeComboBox.Trim() : "";
-            return !(RoomtTypeComboBox == "");
-        }
+
     }
 }
--- HospitalManagementSystem/Views/Home.xaml.cs
+++ HospitalManagementSystem/Views/Home.xaml.cs
@@ -33,7 +33,7 @@ namespace HospitalManagementSystem.Views
 
         private void navigateToDepartments(object sender, MouseButtonEventArgs e)
         {
-            ViewModel.Content = new EmployeeDetailsVeiwModel();
+            ViewModel.Content = new DepartmentsViewModel();
         }
 
         private void navigateToEmployees(object sender, MouseButtonEventArgs e)
