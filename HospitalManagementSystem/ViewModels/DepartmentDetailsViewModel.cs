c8ce6ae9f7b7a1092f30a20bee1acfa077796f4c
mariamkamel <mariammkamel98@gmail.com>
2018-04-26 14:57:12 +0200

    Department Detials View added

---
+++ HospitalManagementSystem/ViewModels/DepartmentDetailsViewModel.cs
@@ -0,0 +1,42 @@
+﻿using HospitalManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagementSystem.ViewModels
+{
+    public class DepartmentDetailsViewModel: BaseViewModel
+    {
+        public ObservableCollection<String> DoctorsList { get; set; }
+        public ObservableCollection<String> NursesList { get; set; }
+        public ObservableCollection<String> PatientsList { get; set; }
+        public String DepartmentName { set; get; }
+        public String HeadName { set; get; }
+        public List<ComboBoxPairs> DoctorsComboBoxItems;
+        public List<ComboBoxPairs> NursesComboBoxItems;
+        public List<ComboBoxPairs> PatientsComboBoxItems;
+
+        public DepartmentDetailsViewModel()
+        {
+            DoctorsComboBoxItems = new List<ComboBoxPairs>();
+            foreach (Employee employee in Hospital.Employees.Values)
+            {
+                if(employee.GetType()==typeof(Doctor))
+                    DoctorsComboBoxItems.Add(new ComboBoxPairs(employee.ID, employee.Name));
+            }
+
+            NursesComboBoxItems = new List<ComboBoxPairs>();
+            foreach (Employee employee in Hospital.Employees.Values)
+            {
+                if (employee.GetType() == typeof(Nurse))
+                    NursesComboBoxItems.Add(new ComboBoxPairs(employee.ID, employee.Name));
+            }
+
+        }
+
+
+    }
+}
---
+++ HospitalManagementSystem/Views/DepartmentDetailsView.xaml.cs
@@ -0,0 +1,58 @@
+﻿using HospitalManagementSystem.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using MaterialDesignThemes.Wpf;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HospitalManagementSystem.Views.Components
+{
+    /// <summary>
+    /// Interaction logic for DepartmentDetailsView.xaml
+    /// </summary>
+    public partial class DepartmentDetailsView : UserControl
+    {
+
+        public DepartmentDetailsViewModel ViewModel { get; set; }
+
+        public DepartmentDetailsView()
+        {
+            ViewModel = new DepartmentDetailsViewModel();
+            DataContext = ViewModel;
+            InitializeComponent();
+            DepartmentDoctorComboBox.DisplayMemberPath = "Value";
+            DepartmentDoctorComboBox.SelectedValuePath = "Key";
+            DepartmentDoctorComboBox.ItemsSource = ViewModel.DoctorsComboBoxItems;
+
+            DepartmentNursesComboBox.DisplayMemberPath = "Value";
+            DepartmentNursesComboBox.SelectedValuePath = "Key";
+            DepartmentNursesComboBox.ItemsSource = ViewModel.NursesComboBoxItems;
+
+            DepartmentPatientsComboBox.DisplayMemberPath = "Value";
+            DepartmentPatientsComboBox.SelectedValuePath = "Key";
+            DepartmentPatientsComboBox.ItemsSource = ViewModel.PatientsComboBoxItems;
+
+        }
+       
+
+        private void DeleteDepartment(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void EditDepartment(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
