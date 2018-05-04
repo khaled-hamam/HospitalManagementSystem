e3f0d8048ae0dd7a91342e12e365af62e907fb1d
karimelsayed98 <karim.abdelghaffar98@gmail.com>
2018-05-04 15:41:37 +0200

    Added Data to Resident & Appointment Patients And Employees

--- HospitalManagementSystem/ViewModels/DepartmentsViewModel.cs
+++ HospitalManagementSystem/ViewModels/DepartmentsViewModel.cs
@@ -30,20 +30,7 @@ namespace HospitalManagementSystem.ViewModels
 
         public List<ComboBoxPairs> ComboBoxItems;
 
-        public ICommand SearchAction { get; set; }
-
-        public string DepartmentNameTextBox { get; set; }
-        public string DepartmentHeadIDTextBox { get; set; }
-        public bool ValidateNameTextBox()
-        {
-            DepartmentNameTextBox = (DepartmentNameTextBox != null) ? DepartmentNameTextBox.Trim() : "";
-            return !(DepartmentNameTextBox == "");
-        }
-        public bool ValidateHeadIDTextBox()
-        {
-            DepartmentHeadIDTextBox = (DepartmentHeadIDTextBox != null) ? DepartmentHeadIDTextBox.Trim() : "";
-            return !(DepartmentHeadIDTextBox == "");
-        }
+        public ICommand SearchAction { get; set; }       
 
         public DepartmentsViewModel()
         {
--- HospitalManagementSystem/ViewModels/EmployeeCardViewModel.cs
+++ HospitalManagementSystem/ViewModels/EmployeeCardViewModel.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using HospitalManagementSystem.Views;
+using System;
+using System.Windows.Input;
 
 namespace HospitalManagementSystem.ViewModels
 {
     public class EmployeeCardViewModel : BaseViewModel
     {
+        public String ID { get; set; }
         public String Name { get; set; }
         public String Role { get; set; }
         public String Department { get; set; }
         public String Salary { get; set; }
+
+        public ICommand NavigateToDetailsAction { get; set; }
+
+        public EmployeeCardViewModel()
+        {
+            NavigateToDetailsAction = new RelayCommand(navigateToDetails);
+        }
+
+        public void navigateToDetails()
+        {
+            Home.ViewModel.Content = new EmployeeDetailsVeiwModel(ID) {
+                EmployeeName = Name,
+                EmployeeRole = Role,
+                EmployeeSalary = Salary,
+                EmployeeDepartment = Department,
+            };
+        }
     }
 }
