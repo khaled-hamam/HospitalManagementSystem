ca38950427aab7a526cd4e05feb7eec13a57bccc
khaled-hamam <Khal3d.Mohamed@gmail.com>
2018-05-06 19:16:38 +0200

    Fixed Insert Appointment in DB

--- HospitalManagementSystem/Services/HospitalDB.cs
+++ HospitalManagementSystem/Services/HospitalDB.cs
@@ -672,7 +672,7 @@ namespace HospitalManagementSystem.Services
             {
                 con.Open();
                 String query = $"INSERT INTO appointment VALUES('{appointment.ID}', '{appointment.Patient.ID}', " +
-                    $"'{appointment.Doctor.ID}', '{appointment.Date.ToString("yyyy-MM-dd")}', {appointment.Duration}";
+                    $"'{appointment.Doctor.ID}', '{appointment.Date.ToString("yyyy-MM-dd")}', {appointment.Duration})";
                 MySqlCommand command = new MySqlCommand(query, con);
                 await command.ExecuteNonQueryAsync();
             }
