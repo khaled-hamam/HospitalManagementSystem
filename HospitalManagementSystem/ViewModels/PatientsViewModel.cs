f7e7810e09e7573a2740a2ca4f22e7a4c5707cc8
mariamkamel <mariammkamel98@gmail.com>
2018-05-06 19:49:42 +0200

    patient bugs fixed

--- HospitalManagementSystem/ViewModels/PatientsViewModel.cs
+++ HospitalManagementSystem/ViewModels/PatientsViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using HospitalManagementSystem.Models;
 using HospitalManagementSystem.Services;
+using HospitalManagementSystem.Views;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -134,7 +135,7 @@ namespace HospitalManagementSystem.ViewModels
                 Hospital.Patients.Add(newPatient.ID, newPatient);
                 Hospital.Rooms[RoomNumber.Key].Patients.Add(newPatient.ID, newPatient);
                 HospitalDB.InsertPatient(newPatient);
-                 
+
             } else {
                 AppointmentPatient newPatient = new AppointmentPatient
                 {
@@ -161,6 +162,7 @@ namespace HospitalManagementSystem.ViewModels
                 Hospital.Patients.Add(newPatient.ID, newPatient);
                 HospitalDB.InsertPatient(newPatient);
             }
+                Home.ViewModel.CloseRootDialog();
         }
  
     }
